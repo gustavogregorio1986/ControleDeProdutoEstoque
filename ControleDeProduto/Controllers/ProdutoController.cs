4f260cd5c1eb4c1b228632574320c814ecf82cc6
guga56 <gustavogregorio086@gmail.com>
2023-07-25 21:49:07 -0300

    perojeto de desenvolvimento .Net CRUD em estoque de produto layout

---
+++ ControleDeProduto/Controllers/ProdutoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeProduto.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        public IActionResult Consultar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+
+        public IActionResult Alterar()
+        {
+            return View();
+        }
+    }
+}
